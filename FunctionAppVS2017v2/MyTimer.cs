d3d7b6bc3ad32515b512abbfcc3edf8268db03f8
David Ebbo <david.ebbo@gmail.com>
2018-04-21 09:43:26 -0700

    Add timer function

---
+++ FunctionAppVS2017v2/MyTimer.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace FunctionAppVS2017v2
+{
+    public static class MyTimer
+    {
+        static int _counter;
+
+        [FunctionName("MyTimer")]
+        public static void Run(
+            [TimerTrigger("0 0 * * * *")]TimerInfo myTimer,
+            [Queue("myqueue-items-destination")] out string myQueueItemCopy,
+            TraceWriter log)
+        {
+            ++_counter;
+            log.Info($"C# Timer trigger function executed at: {DateTime.Now}, Counter={_counter}, Machine={Environment.MachineName}");
+            myQueueItemCopy = $"{DateTime.Now}, Counter={_counter}, Machine={Environment.MachineName}";
+        }
+    }
+}
